dfe7db01553b3c28fd97a3ba4daf3e1aefc2c8a1
Timilehin Arigbede <Harriemannie@gmail.com>
2022-08-23 16:43:45 +0100

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -30,11 +39,22 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true }) // Don't use this force argument in production
+  // .sync({ force: true }) // Don't use this force argument in production
+  .sync()
   .then((result) => {
+    User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Harrie", email: "harrie@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
-    // console.log(err);
+    console.log(err);
   });
